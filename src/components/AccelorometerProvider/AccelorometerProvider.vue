53d1c3016dac87497591493f9286e391cdd9b845
Ruslan Koldunsky <ruslan.koldunsky@xing.com>
2020-07-03 13:46:15 +0200

    Cube with correct perspective

---
+++ src/components/AccelorometerProvider/AccelorometerProvider.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    [[ AccelorometerProvider ]]
+  </div>
+</template>
+
+<script>
+export default {
+  props: {},
+
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style src="./AccelorometerProvider.scss" lang="scss" scoped />
